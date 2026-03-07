6c503e0ca813443d9a633eb2d581ae755311552d
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-07 14:10:56 +0600

    feat: add ProductCart model with user and product relation

---
+++ app/Models/ProductCart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\User;
+use App\Models\Product;
+
+class ProductCart extends Model
+{
+    use HasFactory;
+
+    //
+    protected $table = 'product_carts';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'price',
+        'color',
+        'size',
+        'qty',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
