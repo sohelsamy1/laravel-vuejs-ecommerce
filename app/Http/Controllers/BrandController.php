55f16fecd6fe40338b8e284b4e7a374096d73e06
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-07 21:34:38 +0600

    feat: add BrandList API with ResponseHelper and route

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Helper\ResponseHelper;
+use App\Models\Brand;
+use Illuminate\Http\JsonResponse;
+
 
 class BrandController extends Controller
 {
-    
+    public function BrandList(): JsonResponse
+    {
+        $data = Brand::select('id', 'brand_name','brand_img')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// Brand
+Route::get('/BrandList', [BrandController::class, 'BrandList']);
